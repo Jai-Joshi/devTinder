220f1974a4cd81ce437dfa83b86bdda808356e4f
Jai-Joshi <jaijoshijj456@gmail.com>
2026-03-31 01:06:39 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -2,6 +2,25 @@ const express = require('express');
 
 const app = express();
 
+app.use("/Namaskar", (req, res) => {
+    res.send("Namaskar World");
+});
+
+app.get("/User", (req,res) =>{
+    console.log("getting user");
+    res.send("Got the user",{"name": "JJ"});
+})
+
+app.post("/User", (req,res) =>{
+    console.log("creating user");
+    res.send("Created the user");
+})
+
+app.delete("/User", (req, res) =>{
+    console.log("deleting user");
+    res.send("Deleted the user");
+})
+
 app.use("/", (req, res) => {
     res.send("Hello World");
 });
