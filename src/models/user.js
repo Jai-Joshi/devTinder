bd76179e8a4e490f1539f7b6d41b454075fd039c
Jai-Joshi <jaijoshijj456@gmail.com>
2026-04-14 19:41:24 +0530

    Addeed more validations

--- src/models/user.js
+++ src/models/user.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const validator = require('validator');
 const userSchema = new mongoose.Schema({
 
     firstName: {
@@ -17,13 +17,23 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
         lowercase: true,
-        trim: true
+        trim: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error("Invalid email address");
+            }
+        }
     },
 
     password: {
         type: String,
         required: true,
-        minlength: 6
+        minlength: 6,
+        validate(value) {
+            if (!validator.isStrongPassword(value)) {
+                throw new Error("Password must be at least 6 characters long and include at least one uppercase letter, one lowercase letter, one number, and one symbol");
+            }
+        }
     },
 
     age: {
@@ -47,13 +57,18 @@ const userSchema = new mongoose.Schema({
 
     photoUrl: {
         type: String,
-        default: "https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Picture.png"
+        default: "https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-Picture.png",
+        validate(value) {
+            if (!validator.isURL(value)) {
+                throw new Error("Invalid URL for photo");
+            }
+        }
     },
 
     skills: {
         type: [String],
-        validate(value){
-            if(value.length > 5){
+        validate(value) {
+            if (value.length > 5) {
                 throw new Error("You can only add up to 5 skills");
             }
         }
